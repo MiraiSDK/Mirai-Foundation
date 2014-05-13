c10a7edae5986d3ff72d1306edbc8d3afc38ceff
Yonghui Chen <chyhfj@gmail.com>
2014-05-13 15:44:31 +0800

    add NSSecureCoding protocol

--- Headers/Foundation/NSObject.h
+++ Headers/Foundation/NSObject.h
@@ -282,6 +282,13 @@ extern "C" {
 - (id) initWithCoder: (NSCoder*)aDecoder;
 @end
 
+@protocol NSSecureCoding <NSCoding>
+@required
+// This method must be return YES on all classes that allow secure coding. Subclasses of classes that adopt NSSecureCoding and override initWithCoder: must also override this method and return YES.
+// The Secure Coding Guide should be consulted when writing methods that decode data.
++ (BOOL)supportsSecureCoding;
+@end
+
 
 GS_ROOT_CLASS @interface NSObject <NSObject>
 {
