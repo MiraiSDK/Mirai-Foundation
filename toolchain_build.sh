0af7baa20e410bcd823cfcd91c7d04c802799aac
Jack <jack@tiny4cocoa.org>
2014-08-10 01:28:09 +0800

    add toolchain_build script

---
+++ toolchain_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+LIBS="-lgnustl_shared" CFLAGS="-DU_STATIC_IMPLEMENTATION" CPPFLAGS="-DU_STATIC_IMPLEMENTATION" ./configure \
+	--prefix=$MIRAI_SDK_PREFIX --host="arm-linux-androideabi" --enable-nxconstantstring \
+	--enable-pass-arguments -disable-xslt --disable-tls \
+	--with-xml-prefix=$MIRAI_SDK_PREFIX --disable-openssl
+
+make debug=yes
+make install debug=yes
+
+pushd $MIRAI_SDK_PREFIX/lib
+mv libgnustep-base.so.1.24.6 libgnustep-base.so
+rm libgnustep-base.so.1.24
+popd
