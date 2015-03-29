c3b7cd429a7a8e7846ed58c5186fb8fae217f466
Yonghui Chen <chyhfj@gmail.com>
2015-03-29 22:17:58 +0800

    update to support x86 target

--- toolchain_build.sh
+++ toolchain_build.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+checkError()
+{
+    if [ "${1}" -ne "0" ]; then
+        echo "*** Error: ${2}"
+        exit ${1}
+    fi
+}
 
-LIBS="-lgnustl_shared" CFLAGS="$ARCHFLAGS -DU_STATIC_IMPLEMENTATION -fblocks" CPPFLAGS="$ARCHFLAGS -DU_STATIC_IMPLEMENTATION -fblocks" LDFLAGS="$ARCHLDFLAGS" ./configure \
-	--prefix=$MIRAI_SDK_PREFIX --host="arm-linux-androideabi" --enable-nxconstantstring \
+
+CC=$CROSS_CLANG LIBS="-lgnustl_shared" CFLAGS="$ARCHFLAGS -DU_STATIC_IMPLEMENTATION -fblocks" CPPFLAGS="$ARCHFLAGS -DU_STATIC_IMPLEMENTATION -fblocks" LDFLAGS="$ARCHLDFLAGS" ./configure \
+	--prefix=$MIRAI_SDK_PREFIX --host="$HOSTEABI" --enable-nxconstantstring \
 	--enable-pass-arguments -disable-xslt --disable-tls \
 	--with-xml-prefix=$MIRAI_SDK_PREFIX --disable-openssl
+checkError $? "configure gnustep-base failed"
 
 make debug=yes
+checkError $? "make gnustep-base failed"
+
 make install debug=yes
+checkError $? "install gnustep-base failed"
+
 
 pushd $MIRAI_SDK_PREFIX/lib
 mv libgnustep-base.so.1.24.6 libgnustep-base.so
 rm libgnustep-base.so.1.24
+ln -s libgnustep-base.so libFoundation.so
 popd
