07671623cdab161396f9f78d107ca2d4de53fd54
Yonghui Chen <chyhfj@gmail.com>
2016-02-29 21:14:40 +0800

    toolchain build use release target

--- toolchain_build.sh
+++ toolchain_build.sh
@@ -15,10 +15,10 @@ CC=$CROSS_CLANG LIBS="-lgnustl_shared" CFLAGS="$ARCHFLAGS -DU_STATIC_IMPLEMENTAT
 	--with-xml-prefix=$MIRAI_SDK_PREFIX --disable-openssl --with-default-config=./Android_GNUstep.conf
 checkError $? "configure gnustep-base failed"
 
-make debug=yes
+make -j4 #debug=yes
 checkError $? "make gnustep-base failed"
 
-make install debug=yes
+make install #debug=yes
 checkError $? "install gnustep-base failed"
 
 
